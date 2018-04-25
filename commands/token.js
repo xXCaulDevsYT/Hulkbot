afe5907639a8a54ffcafc90176a4f9b645ce255d
FHGDev <gideon.lingle14@gmail.com>
2018-04-25 15:34:13 -0500

    Update token.js

--- commands/token.js
+++ commands/token.js
@@ -1,7 +1,7 @@
 module.exports.run = (bot, message, args) => {
   let member = message.mentions.members.first();
-  message.channel.send(`LOL! ${member.username} got trolled by ${message.author.username}`)
-  console.log(`ID: ${member.id}`)
+  message.channel.send(`LOL! ${member.username} got trolled by ${message.author.username}.`)
+  message.channel.send(`LOL! ${member.username} got trolled by ${message.member.username}.`)
 }
 
 module.exports.help = {
