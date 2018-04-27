b7f4483245f05a1a4dafffb796dcf3cd0480378b
FHGDev <gideon.lingle14@gmail.com>
2018-04-27 15:28:37 -0500

    Update BanRemove.js

--- events/BanRemove.js
+++ events/BanRemove.js
@@ -1,21 +1,18 @@
 const discord = require('discord.js')
 
 module.exports = (bot, guild, member) => {
-  member.send(`${member.username}, you have now been unbanned from ${guild.name}!`)
-  const lognames = ["guild-maintenance", "log", "bot-log", "hulkbot-log"]
-  
-  guild.channels.forEach(channel => {
-    if (lognames.includes(channel.name)) {
-      let embed = new discord.RichEmbed()
+  member.send(`${member.user.username}, you have now been unbanned from ${guild.name}!`)
+  let channel = guild.channels.find('name', 'guild-bot-log')
+  if (!channel) {
+    console.warn(`Guild ${guild.name} has no log channel, canceling send.`);
+  } else {
+    let embed = new discord.RichEmbed()
       .setTitle("Hulkbot Logger")
-      .setDescription(`${member.username} was just unbanned from ${guild.name}`)
+      .setDescription(`${member.user.username} was just unbanned from ${guild.name}`)
       .setTimestamp()
       .setColor("BLUE")
-      .setThumbnail(member.avatarURL)
-      .setFooter(`${member.username} unbanned`)
+      .setThumbnail(member.user.avatarURL)
+      .setFooter(`${member.user.username} unbanned`)
       channel.send({embed: embed})
-    } else {
-      return;    
-    }
-  })
+  }
 }
