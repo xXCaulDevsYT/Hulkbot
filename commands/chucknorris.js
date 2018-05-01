cb77d0032b9c48fc28289b200f9006e0be1db539
FHGDev <gideon.lingle14@gmail.com>
2018-04-30 23:28:02 -0500

    Update chucknorris.js

--- commands/chucknorris.js
+++ commands/chucknorris.js
@@ -1,15 +1,15 @@
 const snekfetch = require('snekfetch')
 
 module.exports.run = (bot, message, args, discord) => {
-  snekfetch.get('http://api.icndb.com', function(err, body) {
-    let em = new discord.RichEmbed()
-    .setTitle(`Hulkbot Chuck Norris Jokes`)
-    .setDescription(JSON.stringify(body.joke))
-    .setColor("RANDOM")
-    .setThumbnail(bot.user.avatarURL)
-    
-    message.channel.send({ em })
-  }) 
+  snekfetch.get('https://api.chucknorris.io/jokes/random') 
+  .then(response => {
+      let em = new discord.RichEmbed()
+      .setTitle(`Hulkbot Chuck Norris Jokes`)
+      .setDescription(response.body.value)
+      .setColor("RANDOM")
+      .setThumbnail(response.body.icon_url)
+      message.channel.send({ em })
+  })
 }
 
 module.exports.help = {
