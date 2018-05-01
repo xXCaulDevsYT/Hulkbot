1356f382fa1a59be0c4064de6eef93501e848530
FHGDev <gideon.lingle14@gmail.com>
2018-04-30 19:33:13 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const config = require('../config.json')
 
 module.exports.run = (bot, message, args, discord) => {
+    if (!message.member.hasPermission(["BAN_MEMBERS"])) return message.channel.send("You don't have permission to ban people. If there's someone who needs to be banned, contact an administrator or a moderator.")
     let reason = args.slice(1).join(" ");
     let member = message.mentions.members.first();
     member.ban(reason).catch(console.error)
