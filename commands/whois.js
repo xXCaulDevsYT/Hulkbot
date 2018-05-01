7bd54bdba6ef71a622a9256a313ab963c07318b0
FHGDev <gideon.lingle14@gmail.com>
2018-05-01 00:44:35 -0500

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -3,12 +3,14 @@ let snekfetch = require('snekfetch')
 module.exports.run = (bot, message, args, discord) => {
   let member = message.mentions.members.first();
   if (!member) return message.channel.send("You need to mention someone.")
-    message.channel.send(`Okay, now looking up ${member.user.username} on ROBLOX...`).then(m => m.delete(5000))
     snekfetch.get(`https://verify.eryn.io/api/user/${member.id}`)
     .then(response => {
       if (response.body.status == "ok") {
+         message.channel.send(`Okay, now looking up ${member.user.username} on ROBLOX...`).then(m => m.delete(5000))
+        setTimeout(() => {
         message.channel.send(`Done! I found ${member.user.username} on ROBLOX! His/her username is ${response.body.robloxUsername}, and his/her ID is ${response.body.robloxId}.`)
       } 
+    }, 5000)
       if (response.body.status == "error") {
         message.channel.send(`I couldn't find ${member.user.username} on ROBLOX...`)
       }
