0e63919afc28abe347c9fb3a1ec70db0c3aff6ae
FHGDev <gideon.lingle14@gmail.com>
2018-05-01 00:00:15 -0500

    Create timer.js

---
+++ commands/timer.js
@@ -0,0 +1,19 @@
+module.exports.run = (bot, message, args, discord) => {
+  let time = args.join(' ')
+  let embed = new discord.RichEmbed()
+  .setTitle("Hulkbot Timer")
+  .setDescription("Okay, I'll count down, and let you know when the time is up!")
+  .setTimestamp()
+  .setColor("RANDOM")
+  .setFooter("Timer")
+  message.channel.send({embed: embed})
+  
+  setTimeout(() => {
+    embed.setDescription(`BEEP! The time's up! <@${message.author.id}>`)
+    message.channel.send({embed: embed})
+  }, time)
+}
+
+module.exports.help = {
+  name: "timer"
+}
