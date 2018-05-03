23594640e3d9f556cf543355fa134847232efb33
FHGDev <gideon.lingle14@gmail.com>
2018-05-03 17:11:06 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -2,7 +2,7 @@ module.exports.run = (bot, message, args, discord) => {
   var snekfetch = require('snekfetch')
   snekfetch.post('https://discordbots.org/api/bots/stats')
   .set('Authorization', process.env.tok)
-  .send({ server_count: "1000" })
+  .send({ server_count: bot.guilds.size })
   .then(() => {
     console.log("Updated DBL Stats.")
     message.channel.send("Updated DBL Stats.")
