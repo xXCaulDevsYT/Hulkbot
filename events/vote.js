4acc7fe7a72578d722cd83ff79292a0975969963
FHGDev <gideon.lingle14@gmail.com>
2018-05-03 16:07:05 -0500

    Update vote.js

--- events/vote.js
+++ events/vote.js
@@ -1,9 +1,10 @@
 const DBL = require('dblapi.js');
-const dbl = new DBL(process.env.tok, { webhookPort: 5000, webhookAuth: process.env.whpassword });
+const dbl = new DBL(process.env.tok, { webhookPort: process.PORT, webhookAuth: process.env.whpassword });
 dbl.webhook.on('ready', hook => {
   console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
 });
 dbl.webhook.on('vote', vote => {
-  console.log(vote);
+  console.log("LOOL");
+  exports = vote
   // Do what you need to do
 });
