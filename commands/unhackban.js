cc0a474c050a185fe2aa6a41ca2f875a7e4556eb
FHGDev <gideon.lingle14@gmail.com>
2018-05-05 19:23:24 -0500

    Update unhackban.js

--- commands/unhackban.js
+++ commands/unhackban.js
@@ -2,9 +2,9 @@ module.exports.run = (bot, message, args, discord) => {
   let id = args.join(' ');
   let member = bot.fetchUser(id).then(user => {
     message.guild.unban(user.id).catch(err => {
-      message.channel.send(`Failed to unban ${user.username}`)
+      message.channel.send(`Failed to unban ${user}`)
     })
-    message.channel.send(`Alright, I unbanned ${user.username}.`)
+    message.channel.send(`Alright, I unbanned ${user}.`)
   }).catch(() => message.channel.send("Sorry, I can't find a user with that ID..."))
 }
   
