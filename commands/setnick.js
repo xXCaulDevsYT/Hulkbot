8ea2cd07365fbb619711c6979a7b51940ce5aae4
FHGDev <gideon.lingle14@gmail.com>
2018-05-05 19:10:36 -0500

    Update setnick.js

--- commands/setnick.js
+++ commands/setnick.js
@@ -1,14 +1,15 @@
 module.exports.run = (bot, message, args, discord) => {
-  if (message.author.id == process.env.oid) {
-  let newname = args.join(' ')
-  bot.user.setUsername(newname)
+if (message.author.id == process.env.oid) {
+    let newname = args.join(' ')
+    bot.user.setUsername(newname)
   
-  message.channel.send(`Okay, I set my username to ${newname}!`).then(m => {
-    message.delete(100)
-    m.delete(100)
-  })
-  } else {
-    message.channel.send("Nope!")
+    message.channel.send(`Okay, I set my username to ${newname}!`).then(m => {
+      message.delete(100)
+      m.delete(100)
+    })
+    } else {
+      message.channel.send("Nope!")
+    }
   }
 }
 
