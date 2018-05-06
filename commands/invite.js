48647d8d0c20a8aa4585fcd01065a54aa558a402
FHGDev <gideon.lingle14@gmail.com>
2018-05-05 19:45:14 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,10 +5,11 @@ module.exports.run = (bot, message, args) => {
   .setTitle("Hulkbot Invitation")
   .setDescription(`I can't use invite links.\nHere's an OAuth2 link instead! Click [here](https://discordapp.com/api/oauth2/authorize?client_id=294194506113220608&permissions=8&redirect_uri=https%3A%2F%2Fbot.hulkbot.ml%2Fhome&response_type=code&scope=bot%20guilds) to invite me!`)
   .setColor("GREEN")
-  .setFooter(`This command was used at ${new Date}`)
-  message.channel.send({ embed })
+  .setFooter(`Invite Command`)
+  .setTimestamp()
+  message.channel.send({embed: embed})
 }
 
 module.exports.help = {
-  name: "invite",
+  name: "invite"
 }
