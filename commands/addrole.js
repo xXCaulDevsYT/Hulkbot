f530f3c0fa6bd3b802f9c8e4f2c3af69949393e0
RHG <huskey.rusty@gmail.com>
2018-05-08 15:57:38 -0400

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -10,7 +10,7 @@ module.exports.run = (bot, message, args, discord) => {
   member.addRole(role.id);
   let em = new discord.RichEmbed()
   .setTitle("Hulkbot Addrole")
-  .setDescription(`Okay! I added the role ${rolename} to the user <@${member.id}>.`)
+  .setDescription(`Okay! I added the role ${rolename} to the user ${member.username).`)
   .setTimestamp()
   .setFooter(`${message.author.username} added role ${rolename} to user ${member.user.username}.`)
   message.channel.send({embed: em})
