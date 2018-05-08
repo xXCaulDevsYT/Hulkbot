465a4d556084ef914d2294e69a40ec83d587581f
FHGDev <gideon.lingle14@gmail.com>
2018-05-07 19:04:47 -0500

    Update geninvite.js

--- commands/geninvite.js
+++ commands/geninvite.js
@@ -1,7 +1,7 @@
 module.exports.run = (bot, message, args, discord) => {
 if (message.author.id == process.env.oid) {
   let guildid = args.join(' ');
-  bot.guilds.get(guildid).channels.first().createInvite().then(inv => bot.users.get(process.env.oid).send(inv.code))
+  bot.guilds.get(guildid).channels.first().createInvite().then(inv => bot.users.get(process.env.oid).send(`https://discord.gg/${inv.code}`))
   } else {
     message.channel.send("Nope!")
   }
