09d931bc0b66b0dca17089a8de0ba6a56041fb0b
FHGDev <gideon.lingle14@gmail.com>
2018-05-08 09:46:51 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,26 @@ module.exports.run = (bot, message, args, discord) => {
 	let mod = ["ban", "hackban", "unhackban", "softban", "kick", "mute", "unmute", "purge"]
 	let nsfw = ["pussy", "ass", "boobs", "dick", "fuck"]
 	let msg = args.join(" ")
+	if (!categories.includes(msg)) return message.channel.send("That's not a category, please enter a valid category.")
 	let em = new discord.RichEmbed()
 	.setTitle("Help Menu")
 	.setFooter(`Requested by ${message.author.username}.`)
+	.setColor("RANDOM")
+	
+	if (msg == categories[4]) {
+		em
+		.setTitle(":heavy_plus_sign: More help")
+		.setDescription("For more help, check out the [command docs](https://gist.github.com/FHGDev/67ed143f693a6424cc4ef7ce9ca3c796)!")
+		.addField("Do you *still* need more help? Use the `h!contact` command, and I'll get to you ASAP.")
+		
+		message.channel.send({embed: em})
+	}
 	
 	if (msg == categories[2]) {
 		em
 		.setTitle(":lollipop: Fun")
 		.setDescription("Let's get some fun going in this boring place!")
-		.addField("Fun Commands", fun.join("\n"), true)
+		.addField("Fun Commands", `**${fun.join("\n")}**`, true)
 		
 		message.channel.send({embed: em})
 	}
@@ -21,7 +32,7 @@ module.exports.run = (bot, message, args, discord) => {
 		em
 		.setTitle("Moderation :hammer_pick:")
 		.setDescription("Let me handle the bad bois... Hehe.")
-		.addField("Moderation Commands", mod.join("\n"), true)
+		.addField("Moderation Commands", `**${mod.join("\n")}**`, true)
 		
 		message.channel.send({embed: em})
 	}
@@ -29,7 +40,7 @@ module.exports.run = (bot, message, args, discord) => {
 		em
 		.setTitle("Bot :robot:")
 		.setDescription(`Get to kno da wae... UMMM I mean the bot.`)
-		.addField("Bot Commands", bota.join("\n"), true)
+		.addField("Bot Commands", `**${bota.join("\n")}**`, true)
 		
 		message.channel.send({embed: em})
 	}
@@ -37,7 +48,7 @@ module.exports.run = (bot, message, args, discord) => {
 		em
 		.setTitle("NSFW :underage:")
 		.setDescription(`These commands are *not* for children!`)
-		.addField(`NSFW Commands`, nsfw.join("\n"), true) 
+		.addField(`NSFW Commands`, `${nsfw.join("\n")}**`, true) 
 		
 		message.channel.send({embed: em})
 	}
