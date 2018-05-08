a96df326911b1068ba84c8bc26383aaf364e652d
FHGDev <gideon.lingle14@gmail.com>
2018-05-07 20:08:09 -0500

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,11 +1,11 @@
 module.exports.run = (bot, message, args, discord) => {
   let role = message.mentions.roles.first();
+  if (!role) return message.channel.send(`You need to mention a role.`);
   let member = message.mentions.members.first();
   if (!member) return message.channel.send("You need to mention someone.");
   let roleid = role.id;
   let rolename = role.name;
   
-  if (!role) return message.channel.send(`You need to mention a role.`);
   if (!message.guild.roles.get(roleid)) return message.channel.send(`That role doesn't exist...`);
   member.addRole(role.id);
   let em = new discord.RichEmbed()
