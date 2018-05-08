2cea35865dfc087381579833f8eaec863e8033ff
FHGDev <gideon.lingle14@gmail.com>
2018-05-07 22:33:06 -0500

    Update ass.js

--- commands/ass.js
+++ commands/ass.js
@@ -1,24 +1,22 @@
-const request = require("snekfetch")
-const fs = require("fs")
+const puppy = require('random-puppy')
 
 module.exports.run = (bot, message, args, discord) => {
-  var max = 12449;
-    var min = 10000;
-    var MathRan = Math.floor(Math.random() * (max - min + 0)) + min;
-    var MathLoL = Math.round(MathRan);
-    if (!message.channel.nsfw) {
-        message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
-    } else {
-        request.get("http://media.obutts.ru/butts_preview/" + MathLoL + ".jpg").then(r => {
-            let img = fs.writeFileSync(`ass.jpg`, r.body)
-            let em = new discord.RichEmbed()
-            .setTitle("Hulkbot Ass")
-            .setDescription("Here's an ass pic...")
-            .setImage(img)
-            message.channel.send({embed: em})
-            fs.unlink(`./ass.jpg`)
-        })
-    }
+  let keywords = [
+    "ass",
+    "butt",
+    "asshole",
+    "pussy",
+    "butthole"
+  ]
+  
+  puppy(keywords).then(url => {
+    let embed = new discord.RichEmbed()
+    .setTitle("Hulkbot Ass")
+    .setDescription("Here's an ass pic...")
+    .setImage(url)
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.username`)
+  })
 }
 
 module.exports.help = {
