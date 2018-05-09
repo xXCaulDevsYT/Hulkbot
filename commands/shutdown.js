73e3d68cb8ece70e6a129e6808294c23729ff039
FHGDev <gideon.lingle14@gmail.com>
2018-05-09 11:12:46 -0500

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,15 +1,12 @@
-const config = require('../config.json')
-const discord = require('discord.js')
-
-module.exports.run = (bot, message, args) => {
-  if (message.author.id == config.ownerid) {
+module.exports.run = (bot, message, args, discord) => {
+  if (message.author.id == process.env.oid) {
     let embed = new discord.RichEmbed()
       .setTitle("Hulkbot Shutdown")
       .setDescription(":wave: Hulkbot will now shutdown... :cry:")
       .setThumbnail(bot.user.avatarURL)
       .setColor('BLUE')
     message.channel.send({ embed })
-    setTimeout(function() {
+    setTimeout(() => {
       process.exit(666);
     }, 500)
   } else {
