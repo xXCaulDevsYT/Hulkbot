b20b6207c4fe1765b7ab8d6fbe28986db8d1314f
FHGDev <gideon.lingle14@gmail.com>
2018-05-09 17:12:52 -0500

    Update BanRemove.js

--- events/BanRemove.js
+++ events/BanRemove.js
@@ -2,7 +2,7 @@ const discord = require('discord.js')
 const names = ["bot-logs", "log", "hulkbot-log", "bot-hell"]
 module.exports = (bot, guild, member) => {
   guild.channels.forEach(channel => {
-    if (channel.topic.toLowercase().includes("bot log") || channel.name == names[0] || channel.name == names[1] || channel.name == names[2]) {
+    if (channel.topic == "bot log" || channel.name == names[0] || channel.name == names[1] || channel.name == names[2]) {
       const logchannel = channel
       const embed = new discord.RichEmbed()
       .setTitle("Hulkbot Ban Logger")
