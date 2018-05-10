fec95bab2e8fed3e15beab68bc42917ccaeb4252
FHGDev <gideon.lingle14@gmail.com>
2018-05-10 14:10:59 -0500

    Create alert.js

---
+++ commands/alert.js
@@ -0,0 +1,24 @@
+module.exports.run = (bot, message, args) => {
+let msg = args.join(" ");
+if (message.author.id !== message.guild.ownerid) {
+  message.guild.members.forEach(member => {
+    if (member.hasPermission(["ADMINISTRATOR"]) || member.hasPermission(["MANAGE_SERVER"])) {
+      message.channel.send("Alerting admins...")
+      if (!msg) {
+        member.send(`${message.member.user.username} is calling for you in server ${message.guild.name}.`)
+        message.channel.send(`Alerted ${member.user.username}.`)
+      } else {
+        member.send(`${message.author.username} is calling for you in server ${message.guild.name}. Message: ${msg}`)
+        message.channel.send(`Alerted ${member.user.username}.`)
+      }
+    }
+  })
+  } else {
+    message.channel.send("Only the guild member can use this command.");
+  }
+}
+
+module.exports.help = {
+  name: "alert",
+  usage: "h!alert [msg]"
+}
