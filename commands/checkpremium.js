a55f47bd31ad7013211d108277c8750f6d9bb2cc
FHGDev <gideon.lingle14@gmail.com>
2018-05-09 21:36:35 -0500

    Update checkpremium.js

--- commands/checkpremium.js
+++ commands/checkpremium.js
@@ -6,31 +6,31 @@ module.exports.run = (bot, message, args, discord) => {
     let ga = bot.guilds.get('356178662837452800')
     let member = message.author
     if (message.member == message.guild.owner) {
-    if (ga.members.find('id', member.id).roles.find('name','SuperPatron')) {
+    if (ga.members.get(member.id).roles.find('name','SuperPatron')) {
         
             let em = new discord.RichEmbed()
-            .setTitle("Hulkbot Premium")
-            .setAuthor(`:money_mouth:`)
+            .setTitle(":money_mouth: Hulkbot Premium :money_mouth:")
             .setDescription(`:moneybag: Your premium status is active! Thanks for donating! :moneybag:`)
             .setThumbnail(bot.user.avatarURL)
             .setColor("GREEN")
+            .setTimestamp()
             message.channel.send(em)
     } else {
         let em = new discord.RichEmbed()
-        .setTitle("Hulkbot Premium")
-        .setAuthor(`:money_mouth:`)
+        .setTitle(":money_mouth: Hulkbot Premium :money_mouth:")
         .setDescription(`:thumbsdown: Your premium status is unactive. Buy premium [here!](https://www.patreon.com/bePatron?c=1365207 "buy premium") :thumbsdown:`)
         .setThumbnail(bot.user.avatarURL)
         .setColor("RED")
+        .setTimestamp()
         message.channel.send(em)
     }
   } else {
       let em = new discord.RichEmbed()
       .setTitle("Failure! :face_palm:")
       .setDescription("You need to be the owner of this guild to use this command.")
-      .setAuthor(`:money_mouth:`)
       .setThumbnail(bot.user.avatarURL)
       .setColor("RED")
+      .setTimestamp()
       
     message.channel.send({embed: em})
   }
