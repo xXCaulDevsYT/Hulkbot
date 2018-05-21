749ebf2c29150eb5114aacfe9cb2a1d0ce449d95
FHGDev <gideon.lingle14@gmail.com>
2018-05-21 18:45:56 -0500

    Update perms.js

--- commands/perms.js
+++ commands/perms.js
@@ -1,7 +1,27 @@
-const mainroles = ["", "", ""]
+const config = require('../config.json')
+const admins = config.admins
 
-module.exports.run = (bot, message, args) => {
-  
+module.exports.run = (bot, message, args, discord) => {
+  if (message.author.id == process.env.oid) {
+    let em = new discord.RichEmbed()
+    .setTitle("Hulkbot Perms")
+    .setDescription("Your user ID is set as `process.env.oid`, you have full perms.")
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.username}.`)
+    .setColor("RANDOM")
+    message.author.send({embed: em})
+  }
+  if (admins.includes(message.author.id)) {
+    let em = new discord.RichEmbed()
+    .setTitle("Hulkbot Perms")
+    .setDescription("Your user ID is in the admin list, you have admin permissions.")
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.username}.`)
+    .setColor("RANDOM")
+    message.author.send({embed: em}))
+  } else {
+    message.author.send("You are a standard user.")
+  }
 }
 
 module.exports.help = {
