b449374dd7b1afd2902bfeb6788fbcf0e427acaf
FHGDev <gideon.lingle14@gmail.com>
2018-05-22 09:23:14 -0500

    Update BanRemove.js

--- events/BanRemove.js
+++ events/BanRemove.js
@@ -2,12 +2,14 @@ const discord = require('discord.js')
 const names = ["bot-logs", "log", "hulkbot-log", "bot-hell"]
 module.exports = (bot, guild, member) => {
   guild.channels.forEach(channel => {
-    if (channel.topic == "bot log" || channel.name == names[0] || channel.name == names[1] || channel.name == names[2]) {
+    if (channel.topic.includes("bot log") || channel.name == names[0] || channel.name == names[1] || channel.name == names[2]) {
       const logchannel = channel
       const embed = new discord.RichEmbed()
       .setTitle("Hulkbot Ban Logger")
       .setDescription(`${member.user.username} was unbanned from the server.`)
       .setFooter(`${member.user.username} unbanned from server.`)
+      .setTimestamp()
+      .setColor("GREEN")
       logchannel.send({embed: embed})
     } else {
       console.warn("No log channel, canceling.")
