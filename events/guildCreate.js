3b1f6015ec21cb5da613476b7f326b65e0747ab2
FHGDev <gideon.lingle14@gmail.com>
2018-05-22 09:30:50 -0500

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,10 +6,10 @@ module.exports = (bot, guild, discord) => {
   .setTitle("Introduction to Hulkbot")
   .addField("Heyo! Thanks for adding me.","Before you start using me, I need to get some things straight. First of all, for some basic info about me, say `h!info`.")
   .addField("To see a list of commands","Say `h!help`.", true)
-  .addField("If you need to report bug:","go to the [issue tracker](https://github.com/FHGDev/JSHulkbot/issues/new), or just use the `h!contact` command, and it will send a message straight to my creator.")
+  .addField("If you need to report a bug:","go to the [issue tracker](https://github.com/FHGDev/JSHulkbot/issues/new), or just use the `h!contact` command, and it will send a message straight to my creator.")
   .addField("Done!", "Alrighty! Now you can start using all my awesome features in your server!")
   .setThumbnail(bot.user.avatarURL)
   .setTimestamp()
-  
-  bot.users.get(guild.ownerID).send({embed})
+ 
+  member.send({embed})
 }
