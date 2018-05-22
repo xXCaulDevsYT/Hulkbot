0c65c9f4e9e1728d49864cbba86f364a890f57f4
FHGDev <gideon.lingle14@gmail.com>
2018-05-22 09:50:04 -0500

    Update alert.js

--- commands/alert.js
+++ commands/alert.js
@@ -3,7 +3,7 @@ let msg = args.join(" ");
 if (message.author.id == message.guild.ownerID) {
    message.channel.send("Alerting admins...")
   message.guild.members.forEach(member => {
-    if (member.hasPermission("ADMINISTRATOR") || member.hasPermission("MANAGE_GUILD") || !member.user.bot) {
+    if (member.hasPermission("ADMINISTRATOR") || member.hasPermission("MANAGE_GUILD") && !member.user.bot) {
       if (!msg) {
         member.send(`${message.author.username} is calling for you in server ${message.guild.name}.`)
         message.channel.send(`Alerted ${member.user.username}.`)
