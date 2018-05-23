a392b88f1c20772aeeafb0add3786df173193bb5
FHGDev <gideon.lingle14@gmail.com>
2018-05-23 14:51:48 -0500

    Update boobs.js

--- commands/boobs.js
+++ commands/boobs.js
@@ -13,6 +13,8 @@ module.exports.run = (bot, message, args, discord) => {
     "nipple",
     "bust"
   ]
+  
+  if (!message.channel.nsfw) return message.channel.send(":underage: You need to be in an NSFW channel to use this command.");
   let res = key[Math.floor(Math.random()*key.length)]
   
   search(res).then(url => {
