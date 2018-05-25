b6602cffdc04c56fb5383c4855c6b6fcdb5190cb
FHGDev <gideon.lingle14@gmail.com>
2018-05-25 12:20:52 -0500

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,17 +1,22 @@
+const discord = require('discord.js');
+
 module.exports.run = (bot, message, args) => {
-  var msgArray = [];
-  msgArray.push(`**${bot.user.username} Uptime**\n`);
   var hrs = Math.round(bot.uptime / (1000 * 60 * 60)) + " hour(s),"
   var mins = " " + Math.round(bot.uptime / (1000 * 60)) % 60 + " minute(s), "
   var sec = Math.round(bot.uptime / 1000) % 60 + " second(s)"
   if (hrs == "0 hour(s),") hrs = ""
   if (mins == " 0 minute(s), ") mins = ""
-  msgArray.push(hrs + mins + sec)
-  message.channel.send(msgArray).then(m => m.delete(15000))
+  let uptime = hrs+mins+sec
+  
+  let em = new discord.RichEmbed()
+  .setTitle(`**${bot.user.username} Uptime**\n`)
+  .setDescription(`**Serving ${bot.guilds.size} for ${uptime}!**`)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setFooter(`Requested by ${message.author.username}.`)
+  message.channel.send({embed: em})
 }
 
 module.exports.help = {
-  name: "uptime",
-  usage: ``,
-  information: ""
+  name: "uptime"
 }
