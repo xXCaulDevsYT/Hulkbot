39758469e2629b382c95f1cd3c0565ecbbaf3101
FHGDev <gideon.lingle14@gmail.com>
2018-07-21 09:44:21 -0500

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,17 @@
+module.exports.help = {
+  name: "8ball"
+}
+
+module.exports.run = (bot, message args, discord) => {
+ let em = new discord.RichEmbed()
+ let results = ["Ask me again", "my sources say yes", "My sources say no", "Try again later", "It's hazy..."] 
+ let res = results[Math.random()*results.length]
+ 
+ em
+ .setTitle("Hulkbot Eight-ball")
+ .setDescription(res)
+ .setTimestamp()
+ .setColor("RANDOM")
+ 
+ message.channel.send({embed: em})
+}
