93ac5be58a84b45a0b19b8270e5f5e4fdbea80f6
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-07-27 13:45:05 -0500

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -5,14 +5,14 @@ module.exports.run = (bot, message, args, discord) => {
   if (codes.includes(msg)) {
     let em = new discord.RichEmbed()
     .setTitle(`${bot.user.displayName} Code Redemption`)
-    .setDescription(`Correct Code! Use this invite to go to the Hulkbot Base discord where you'll get the SuperPatron role, and more!`)
-    .addField(`[Invite](https://discord.gg/qEFNkxB "Click to go to the Hulkbot Base!")`)
+    .setDescription(`Correct Code! Use this invite to go to the Hulkbot Base discord where you'll get the Premium permisions, and more!`)
+    .addField(`[Invite](https://discord.gg/jjsjsosco "Click to go to the Hulkbot Base!")`)
     .setAuthor(`${bot.user.avatarURL} ${bot.user.username}`)
     .setColor(`BLUE`)
     .setTimestamp()
     message.channel.send({embed: em}).then(m => m.delete(5500))
   } else {
-    message.channel.send("Sorry, incorrect code.")
+    message.channel.send("That code might not be correct , or may be mispelled recheck the code and make sure its right & try again.")
   }
 }
 
